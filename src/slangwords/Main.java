93ec66e4116e9d1c6bfebc12a722eebe968c18a6
htnphu <phuhanld@gmail.com>
2022-11-16 20:14:59 +0700

    (files): add initial files

--- src/slangwords/Main.java
+++ src/slangwords/Main.java
@@ -1,8 +1,10 @@
 package slangwords;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
 import java.util.HashMap;
 
 public class Main extends JFrame {
@@ -14,13 +16,42 @@ public class Main extends JFrame {
 
     JPanel contentPanel;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main();
     }
 
-    public Main() {
+    public Main() throws IOException {
+        // load the data at the beginning in order to have faster loading data require
+        loadData();
         addComponents();
         setTitle("Slang Words");
+    }
+
+    public void loadData() throws IOException {
+        // load the data from previous running
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("dictionary.DAT"))){
+            // convert read object to HashMap<String, String>
+            dictionary =  (HashMap<String, String>) ois.readObject();
+            ois.close();
+        } catch (Exception exception) {
+            dictionary = new HashMap<String, String>();
+            try (BufferedReader br = new BufferedReader(new FileReader("slang.txt"))) {
+                // read and skip the first line data
+                br.readLine();
+                String line = ""; // line reading
+                while((line = br.readLine()) != null){
+                    String[] lineSplitData = line.split("`");
+                    // receive the right format of dictionary -> HashMap<String, String>
+                    if (lineSplitData.length == 2) {
+                        // [0]: short hand of slang words, [1]: definition of slang words
+                        dictionary.put(lineSplitData[0], lineSplitData[1]);
+                    }
+                }
+                System.out.println("Read successfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void addComponents() {
@@ -38,6 +69,7 @@ public class Main extends JFrame {
         menu.setBounds(0,0,150,350);
         menu.setBackground(Color.WHITE);
         JButton functionButton01 = new JButton("Find slang words"); functionButton01.setFocusable(false);
+        functionButton01.addActionListener(e -> changeWindowView(new function01(dictionary)));
         JButton functionButton02 = new JButton("Find by definition"); functionButton02.setFocusable(false);
         JButton functionButton03 = new JButton("History"); functionButton03.setFocusable(false);
         JButton functionButton04 = new JButton("Add slang word"); functionButton04.setFocusable(false);
@@ -61,7 +93,6 @@ public class Main extends JFrame {
         contentPanel = new JPanel();
         contentPanel.setBackground(Color.BLUE);
 
-
         JPanel footer = new JPanel();
         JLabel footerText = new JLabel("Hàn Thọ Nhật Phú - 20127591 - Introduction to Java - 20KTPM02");
         footerText.setFont(new Font("Serif", Font.BOLD, 14));
@@ -76,7 +107,36 @@ public class Main extends JFrame {
         frame.add(menu, BorderLayout.WEST);
         frame.add(contentPanel, BorderLayout.CENTER);
         frame.add(footer, BorderLayout.SOUTH);
+
+
+
         frame.setVisible(true);
 
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                windowClosingEventHandler();
+            }
+        });
+    }
+
+    void changeWindowView(JPanel newWindowView) {
+        remove(contentPanel);
+        add(contentPanel = newWindowView);
+        validate();
+    }
+
+    /**
+     * Save data to Dictionary.DAT when user hit close program button in order to allow
+     * program to load the old (previous) data for the user
+      */
+    void windowClosingEventHandler() {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Dictionary.DAT"))){
+            oos.writeObject(dictionary);
+            System.out.println("poo close");
+            oos.close();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
---
+++ src/slangwords/function01.java
@@ -0,0 +1,10 @@
+package slangwords;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class function01 extends JPanel {
+    public function01(HashMap<String, String> dictionary) {
+        System.out.println("poo");
+    }
+}
