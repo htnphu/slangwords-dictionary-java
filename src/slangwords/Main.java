01fa82c199e1ba3d4970b62e832a10c57ba197cf
Phu Han <phuhanld@gmail.com>
2022-11-22 15:30:35 +0700

    (frame & function): have some invisible bugs on the previous layout -> change layout; done function01, other functions still on testing

--- src/slangwords/Main.java
+++ src/slangwords/Main.java
@@ -2,38 +2,37 @@ package slangwords;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.HashMap;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
 
 public class Main extends JFrame {
-
     /**
-     * HashMap give faster searching
+     * Using HashMap in order to enhance the searching time
+     *
      */
     HashMap<String, String> dictionary;
-
     JPanel contentPanel;
-
     public static void main(String[] args) throws IOException {
         new Main();
     }
-
     public Main() throws IOException {
-        // load the data at the beginning in order to have faster loading data require
+        //        // load the data at the beginning in order to have faster loading data require
         loadData();
         addComponents();
         setTitle("Slang Words");
+        setVisible(true);
     }
 
     public void loadData() throws IOException {
         // load the data from previous running
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("dictionary.DAT"))){
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("dictionary.DAT"));
             // convert read object to HashMap<String, String>
-            dictionary =  (HashMap<String, String>) ois.readObject();
+            dictionary = (HashMap<String, String>)ois.readObject();
             ois.close();
-        } catch (Exception exception) {
+        } catch (Exception ex) {
             dictionary = new HashMap<String, String>();
             try (BufferedReader br = new BufferedReader(new FileReader("slang.txt"))) {
                 // read and skip the first line data
@@ -55,87 +54,106 @@ public class Main extends JFrame {
     }
 
     public void addComponents() {
-        JPanel title = new JPanel();
-        title.setBounds(0,0,700,75);
-        title.setBackground(Color.WHITE);
-        JLabel titleText = new JLabel("SLANG WORDS DICTIONARY");
-        titleText.setFont(new Font("Serif", Font.PLAIN, 24));
-        titleText.setHorizontalAlignment(JLabel.CENTER);
-        titleText.setVerticalAlignment(JLabel.CENTER);
-        title.add(titleText);
-
-        JPanel menu = new JPanel();
-        menu.setLayout(new GridLayout(10, 0, 0 ,5));
-        menu.setBounds(0,0,150,350);
-        menu.setBackground(Color.WHITE);
-        JButton functionButton01 = new JButton("Find slang words"); functionButton01.setFocusable(false);
-        functionButton01.addActionListener(e -> changeWindowView(new function01(dictionary)));
-        JButton functionButton02 = new JButton("Find by definition"); functionButton02.setFocusable(false);
-        JButton functionButton03 = new JButton("History"); functionButton03.setFocusable(false);
-        JButton functionButton04 = new JButton("Add slang word"); functionButton04.setFocusable(false);
-        JButton functionButton05 = new JButton("Edit slang word"); functionButton05.setFocusable(false);
-        JButton functionButton06 = new JButton("Delete slang word"); functionButton06.setFocusable(false);
-        JButton functionButton07 = new JButton("Reset"); functionButton07.setFocusable(false);
-        JButton functionButton08 = new JButton("Random a slang"); functionButton08.setFocusable(false);
-        JButton functionButton09 = new JButton("Funny game 1"); functionButton09.setFocusable(false);
-        JButton functionButton10 = new JButton("Funny game 2"); functionButton10.setFocusable(false);
-        menu.add(functionButton01);
-        menu.add(functionButton02);
-        menu.add(functionButton03);
-        menu.add(functionButton04);
-        menu.add(functionButton05);
-        menu.add(functionButton06);
-        menu.add(functionButton07);
-        menu.add(functionButton08);
-        menu.add(functionButton09);
-        menu.add(functionButton10);
-
-        contentPanel = new JPanel();
-        contentPanel.setBackground(Color.BLUE);
-
-        JPanel footer = new JPanel();
-        JLabel footerText = new JLabel("Hàn Thọ Nhật Phú - 20127591 - Introduction to Java - 20KTPM02");
-        footerText.setFont(new Font("Serif", Font.BOLD, 14));
-        footer.add(footerText);
-
         JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.setTitle("Slang words");
-        frame.setSize(700,500);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.add(title, BorderLayout.NORTH);
-        frame.add(menu, BorderLayout.WEST);
-        frame.add(contentPanel, BorderLayout.CENTER);
-        frame.add(footer, BorderLayout.SOUTH);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
 
+        JLabel titleLabel = new JLabel("SLANG WORDS DICTIONARY", JLabel.CENTER);
+        titleLabel.setFont(new Font("Serif", Font.BOLD, 32));
 
-        frame.setVisible(true);
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new BorderLayout());
 
+        JButton functionButton01 = new JButton("Find slang word");
+        functionButton01.setFocusable(false);
+        functionButton01.addActionListener(e -> refreshFrameWithNewContent(new function01(dictionary)));
+
+        JButton functionButton02 = new JButton("Find by definition");
+        functionButton02.setFocusable(false);
+        functionButton02.addActionListener(e -> System.out.println("poo2"));
+
+        JButton functionButton03 = new JButton("History");
+        functionButton03.setFocusable(false);
+        functionButton03.addActionListener(e -> System.out.println("poo3"));
+
+        JButton functionButton04 = new JButton("Add slang word");
+        functionButton04.setFocusable(false);
+        functionButton04.addActionListener(e -> System.out.println("poo4"));
+
+        JButton functionButton05 = new JButton("Edit slang word");
+        functionButton05.setFocusable(false);
+        functionButton05.addActionListener(e -> System.out.println("poo5"));
+
+        JButton functionButton06 = new JButton("Delete slang word");
+        functionButton06.setFocusable(false);
+        functionButton06.addActionListener(e -> System.out.println("poo6"));
+
+        JButton functionButton07 = new JButton("Reset");
+        functionButton07.setFocusable(false);
+        functionButton07.addActionListener(e -> System.out.println("poo7"));
+
+        JButton functionButton08 = new JButton("Random slang");
+        functionButton08.setFocusable(false);
+        functionButton08.addActionListener(e -> System.out.println("poo8"));
+
+        JButton functionButton09 = new JButton("Game 1");
+        functionButton09.setFocusable(false);
+        functionButton09.addActionListener(e -> System.out.println("poo9"));
+
+        JButton functionButton10 = new JButton("Game 2");
+        functionButton10.setFocusable(false);
+        functionButton10.addActionListener(e -> System.out.println("poo10"));
+
+        JPanel buttonContainer = new JPanel();
+        buttonContainer.setLayout(new GridLayout(10, 0, 0, 5));
+        buttonContainer.add(functionButton01);
+        buttonContainer.add(functionButton02);
+        buttonContainer.add(functionButton03);
+        buttonContainer.add(functionButton04);
+        buttonContainer.add(functionButton05);
+        buttonContainer.add(functionButton06);
+        buttonContainer.add(functionButton07);
+        buttonContainer.add(functionButton08);
+        buttonContainer.add(functionButton09);
+        buttonContainer.add(functionButton10);
+
+        // content panel (for changing the view with functions)
+        contentPanel = new JPanel();
+
+        menuPanel.add(buttonContainer, BorderLayout.PAGE_START);
+        mainPanel.add(titleLabel, BorderLayout.PAGE_START);
+        mainPanel.add(contentPanel, BorderLayout.CENTER);
+        mainPanel.add(menuPanel, BorderLayout.LINE_START);
+
+        setPreferredSize(new Dimension(600, 395));
+        setContentPane(mainPanel);
+        pack();
+
+        setVisible(true);
         addWindowListener(new WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent e) {
-                windowClosingEventHandler();
+                windowClosingHandler();
             }
         });
     }
 
-    void changeWindowView(JPanel newWindowView) {
+    void refreshFrameWithNewContent(JPanel newPanelView) {
         remove(contentPanel);
-        add(contentPanel = newWindowView);
+        add(contentPanel = newPanelView);
         validate();
     }
 
-    /**
-     * Save data to Dictionary.DAT when user hit close program button in order to allow
-     * program to load the old (previous) data for the user
-      */
-    void windowClosingEventHandler() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Dictionary.DAT"))){
+    void windowClosingHandler() {
+        // Save dictionary before closing -> history of user
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Dictionary.DAT"));
             oos.writeObject(dictionary);
-            System.out.println("poo close");
             oos.close();
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
